9f50af3126a6648144eab619a2986d140b7f8a11
CandasKuran <candas.kuran@gmail.com>
2023-03-14 21:41:00 +0100

    Initial commit

---
+++ src/app/tools/dialogover.module.ts
@@ -0,0 +1,29 @@
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DialogComponent} from "./dialog.component";
+import {MatInputModule} from "@angular/material/input";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+@NgModule({
+  declarations: [
+    DialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule
+
+  ],
+  providers: [],
+  bootstrap: [],
+})
+export class DialogModule { }
