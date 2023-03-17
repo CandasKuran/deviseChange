e2a2b1ec3a039e1ce32b73b64ee78c88fecb579c
CandasKuran <candas.kuran@gmail.com>
2023-03-17 08:48:11 +0100

    sous-group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class AppComponent {
   ligneData = ELEMENT_DATA;
   groupData = GROUP_DATA;
   columnsToDisplay = ['Designation', 'Qte', 'Unite', 'PU', 'Total'];
+  sousColumnsToDisplay = ['Designation', 'Qte', 'Unite', 'PU', 'Total'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   // @ts-ignore
   expandedElement: GroupChiffrage | PeriodicElement | null;
@@ -47,7 +48,7 @@ export class AppComponent {
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
       this.Designation = result;
-      this.groupData.push({Designation:result, Total:0 });
+      this.groupData.push({Designation:result, Total:0, Qte:0, Unite:0, PU:0 });
       this.table.renderRows();
     });
 
@@ -57,18 +58,34 @@ export class AppComponent {
     this.groupData.pop();
     this.table.renderRows();
   }
-  addDataSousGroup() {
+  addDataSousGroup(row: any) {
     const dialogRef = this.dialog.open(DialogoverComponent, {
       data: {Designation: this.Designation},
     });
-
+  
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
       this.Designation = "======> " + result;
-      this.groupData.push({Designation:this.Designation, Total:0 });
+  
+      // Get index of selected row
+      const index = this.groupData.findIndex(item => item === row);
+  
+      // Add new sub-group to groupData array
+      this.groupData.splice(index + 2, 0, {
+        
+        Designation:this.Designation, 
+        Total: 0,
+        Qte: 0,
+        Unite: 0,
+        PU: 0 
+        
+        
+        
+      });
+  
+      // Render the rows again
       this.sousTable.renderRows();
     });
-
   }
 }
 
@@ -76,33 +93,49 @@ export class AppComponent {
 export interface GroupChiffrage {
   Designation: string;
   Total: number;
+  Qte: number;
+  Unite: number;
+  PU: number;
 }
 
 export interface PeriodicElement {
   Designation: string;
   Qte: number;
   Unite: number;
-  PU: string;
+  PU: number;
   Total: string;
 }
+
 
 
 const GROUP_DATA: GroupChiffrage[] = [
   {
     Designation: "Installation Chauffage",
-    Total: 0
+    Total: 0,
+    Qte: 0,
+    Unite: 0,
+    PU: 0
   },
   {
     Designation: "Material",
-    Total: 0
+    Total: 0,
+    Qte: 0,
+    Unite: 0,
+    PU: 0
   },
   {
     Designation: "Main d'Oeuvre",
-    Total: 0
+    Total: 0,
+    Qte: 0,
+    Unite: 0,
+    PU: 0
   },
   {
     Designation: "Installation sanitaire",
-    Total: 0
+    Total: 0,
+    Qte: 0,
+    Unite: 0,
+    PU: 0
   }
 ]
 
@@ -111,56 +144,56 @@ const ELEMENT_DATA: PeriodicElement[] = [
     Qte: 1,
     Designation: 'Installation chauffage',
     Unite: 1.0079,
-    PU: 'H',
+    PU: 0,
     Total: `---------------------------------`,
   },
   {
     Qte: 2,
     Designation: 'Materiel',
     Unite: 4.0026,
-    PU: 'He',
+    PU: 0,
     Total: `--------------------------------`,
   },
   {
     Qte: 3,
     Designation: 'Radiateur',
     Unite: 6.941,
-    PU: 'Li',
+    PU: 0,
     Total: `------------------------------------`,
   },
   {
     Qte: 4,
     Designation: 'Tuyuaux',
     Unite: 9.0122,
-    PU: 'Be',
+    PU: 0,
     Total: `-----------------------------------`,
   },
   {
     Qte: 5,
     Designation: "Main d'oeuvre",
     Unite: 10.811,
-    PU: 'B',
+    PU: 0,
     Total: `-------------------------------`,
   },
   {
     Qte: 6,
     Designation: 'Installation',
     Unite: 12.0107,
-    PU: 'C',
+    PU: 0,
     Total: `----------------------`,
   },
   {
     Qte: 7,
     Designation: 'Installation Sanitaire',
     Unite: 14.0067,
-    PU: 'N',
+    PU: 0,
     Total: `-------------`,
   },
   {
     Qte: 8,
     Designation: 'douche rendu posé',
     Unite: 15.9994,
-    PU: 'O',
+    PU: 0,
     Total: `--------------------`,
   }
 
